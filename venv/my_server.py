4fef94c28fc0879a057acb66fa839776daeef897
Trisha Sanghal <trishasanghal@Trishas-MacBook-Pro.local>
2019-02-09 23:57:49 -0800

    dict vs list error

--- venv/my_server.py
+++ venv/my_server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import send_from_directory
 from flask import request, url_for, redirect
 import googlemaps
-import Math
+import math
 
 
 app = Flask(__name__, static_folder='')
@@ -10,29 +10,38 @@ app = Flask(__name__, static_folder='')
 
 @app.route('/home')
 def hello_world():
-    print("hitting hello world...")
     return send_from_directory(app.static_folder, 'FrontEnd.html')
 
 @app.route('/gettemp')
 def getTemperatureByLoc():
+    #Get address inputted by user.
     address = request.args.get('loc')
     gmaps = googlemaps.Client(key='AIzaSyC3ORJmeSdB-aN5pqWwypJ0Y9QeK_KnY0U')
+
+    #Convert address into coordinates.
     geocode_result = gmaps.geocode(address)
 
+    #List of all sensors - would eventually read from database.
+    sensors = [
+        ['Pavilion Sensor', gmaps.geocode('The Pavilion Davis, CA 95616')]
+    ]
 
-    # compare location to some table which grabs temperature info
+    #Calculates the distance between two pairs of coordinates.
+    def calc_distance(add, sens):
+        return math.sqrt((add[0] - sens[1]) ** 2 + (add[1] - sens[1]) ** 2)
 
-    # if temp > x , send_from_directory a file that shows that
+    #Find the sensor closest to the user.
+    closest_sensor = sensors[0]
+    min_dist = calc_distance(geocode_result, sensors[0])
+    for s in sensors:
+        dist = calc_distance(geocode_result, s)
+        if dist < min_dist:
+            min_dist = dist
+            closest_sensor = s
 
-    # otherwise, send other file
-
-    # return send_from_directory(app.static_folder, 'DangerResult.html')
-
-    # return redirect(url_for('getDangerResult'))
-    # return app.send_static_file('DangerResult.html')
-
-    return '150'
-
+    #Would eventually read temperature from closest_sensor - currently hardcoded.
+    temperature = str(850)
+    return temperature
 
 @app.route('/dangerresult')
 def getDangerResult():
